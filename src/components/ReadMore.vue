e42ff2f1b0e83ede04561ac22b9873db95aa3970
yunimm <yuu9916@gmail.com>
2022-06-07 01:17:55 +0800

    mobile layout 90%
    -using <img> replace <svg>
    -set up reusable styles

--- src/components/ReadMore.vue
+++ src/components/ReadMore.vue
@@ -3,19 +3,6 @@
     <h2 class="text-right font-bold">
       <slot></slot>
     </h2>
-    <svg
-      width="11"
-      height="18"
-      viewBox="0 0 11 18"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
-        d="M1.87497 3.67326L3.31159 2.25L10.125 9L3.31159 15.75L1.87497 14.3267L7.25173 9L1.87497 3.67326Z"
-        fill="#1F2937"
-      ></path>
-    </svg>
+    <img src="../assets/icons/icon-arrow-SR.svg" alt="right arrow on screen" />
   </div>
 </template>
